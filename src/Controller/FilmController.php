e61c1bd21455142bc70a2d47022e56b0c4f68a44
Groutch <hurizen1@hotmail.com>
2018-08-28 10:21:07 +0200

    first use of bake

---
+++ src/Controller/FilmController.php
@@ -0,0 +1,107 @@
+<?php
+namespace App\Controller;
+
+use App\Controller\AppController;
+
+/**
+ * Film Controller
+ *
+ * @property \App\Model\Table\FilmTable $Film
+ *
+ * @method \App\Model\Entity\Film[]|\Cake\Datasource\ResultSetInterface paginate($object = null, array $settings = [])
+ */
+class FilmController extends AppController
+{
+
+    /**
+     * Index method
+     *
+     * @return \Cake\Http\Response|void
+     */
+    public function index()
+    {
+        $film = $this->paginate($this->Film);
+
+        $this->set(compact('film'));
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id Film id.
+     * @return \Cake\Http\Response|void
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $film = $this->Film->get($id, [
+            'contain' => []
+        ]);
+
+        $this->set('film', $film);
+    }
+
+    /**
+     * Add method
+     *
+     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+     */
+    public function add()
+    {
+        $film = $this->Film->newEntity();
+        if ($this->request->is('post')) {
+            $film = $this->Film->patchEntity($film, $this->request->getData());
+            if ($this->Film->save($film)) {
+                $this->Flash->success(__('The film has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The film could not be saved. Please, try again.'));
+        }
+        $this->set(compact('film'));
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id Film id.
+     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit($id = null)
+    {
+        $film = $this->Film->get($id, [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $film = $this->Film->patchEntity($film, $this->request->getData());
+            if ($this->Film->save($film)) {
+                $this->Flash->success(__('The film has been saved.'));
+
+                return $this->redirect(['action' => 'index']);
+            }
+            $this->Flash->error(__('The film could not be saved. Please, try again.'));
+        }
+        $this->set(compact('film'));
+    }
+
+    /**
+     * Delete method
+     *
+     * @param string|null $id Film id.
+     * @return \Cake\Http\Response|null Redirects to index.
+     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+     */
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $film = $this->Film->get($id);
+        if ($this->Film->delete($film)) {
+            $this->Flash->success(__('The film has been deleted.'));
+        } else {
+            $this->Flash->error(__('The film could not be deleted. Please, try again.'));
+        }
+
+        return $this->redirect(['action' => 'index']);
+    }
+}
---
+++ tests/Fixture/FilmFixture.php
@@ -0,0 +1,62 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * FilmFixture
+ *
+ */
+class FilmFixture extends TestFixture
+{
+
+    /**
+     * Table name
+     *
+     * @var string
+     */
+    public $table = 'film';
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'idFilm' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'titre' => ['type' => 'string', 'length' => 45, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'dateSortie' => ['type' => 'datetime', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'duree' => ['type' => 'time', 'length' => null, 'null' => true, 'default' => null, 'comment' => '', 'precision' => null],
+        'synopsis' => ['type' => 'text', 'length' => null, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null],
+        'DISPO' => ['type' => 'string', 'length' => null, 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['idFilm'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init()
+    {
+        $this->records = [
+            [
+                'idFilm' => 1,
+                'titre' => 'Lorem ipsum dolor sit amet',
+                'dateSortie' => '2018-08-28 07:15:41',
+                'duree' => '07:15:41',
+                'synopsis' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'DISPO' => 'Lorem ipsum dolor sit amet'
+            ],
+        ];
+        parent::init();
+    }
+}
