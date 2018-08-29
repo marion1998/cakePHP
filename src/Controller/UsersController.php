60e5f59cc0ea0b161b3cd2ed42ccf50db7c9c909
Groutch <hurizen1@hotmail.com>
2018-08-29 11:33:29 +0200

    user controller

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -18,6 +18,7 @@ class UsersController extends AppController
         parent::beforeFilter($event);
         $this->Auth->allow('add');
     }
+    
     public function login()
     {
         if ($this->request->is('post')) {
