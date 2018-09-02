d36438814be7ccdee79027411231d120ebea4404
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-09-02 17:37:31 +0200

    giving up with heroku on this

--- src/Application.php
+++ src/Application.php
@@ -46,17 +46,16 @@ class Application extends BaseApplication
             }
 
             $this->addPlugin('Migrations');
-            
         }
 
         /*
          * Only try to load DebugKit in development mode
          * Debug Kit should not be installed on a production system
          */
-    //     if (Configure::read('debug')) {
-    //         $this->addPlugin(\DebugKit\Plugin::class);
-    //     }
-    // }
+        if (Configure::read('debug')) {
+            $this->addPlugin(\DebugKit\Plugin::class);
+        }
+    }
 
     /**
      * Setup the middleware queue your application will use.
