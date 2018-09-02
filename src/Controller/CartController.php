180fa0054170904e6fa73bdaf5192b45a741a0b1
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-09-02 20:31:07 +0200

    clean CartController

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -3,12 +3,7 @@
    use App\Controller\AppController;
 
    class CartController extends AppController{
-      // public function retrieveSessionData(){
-      //    $session = $this->request->session();
       
-      //    $name = $session->read('name');
-      //    $this->set('name',$name);
-      // }
       public function addToCart($id,$title){
          $session = $this->request->session();
          $cart = $session->read('cart');
@@ -25,28 +20,14 @@
                unset($cart[$k]);
             }
          }
-         //$result = array_diff($cart,['id'=>$id]);
          $session->write('cart',$cart);
-         print_r($cart);
          return $this->redirect(['controller'=>'film','action' => 'filmuser']);
       }
-      // public function checkSessionData(){
-      //    $session = $this->request->session();
       
-      //    $name = $session->check('name');
-      //    $address = $session->check('address');
-      //    $this->set('name',$name);
-      //    $this->set('address',$address);
-      // }
       public function deleteCart(){
          $session = $this->request->session();
          $session->delete('cart');
       }
-      // public function destroySessionData(){
-      //    $session = $this->request->session();
-         
-      //    //destroy session
-      //    $session->destroy();
-      // }
+      
    }
 ?>
