1bdef7425107a4d7b2049740539e763db67c74c4
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-09-02 18:19:25 +0200

    fix AppController to allow non connected user to access home.ctp

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -87,7 +87,7 @@ class AppController extends Controller
     }
     public function beforeFilter(Event $event)
     {
-        //$this->Auth->allow(['index', 'view', 'display']);
+        $this->Auth->allow(['display']);
     }
 
     public function beforeRender(Event $event)
